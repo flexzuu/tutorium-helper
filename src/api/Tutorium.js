8dfbf29bb7b6737a3ecfd08e2bc957ea1ff13f15
Jonas Faber <dev.jonas.faber@gmail.com>
2016-10-21 23:24:27 +0200

    Move TutoriumData into own file and location.

--- src/api/Tutorium.js
+++ src/api/Tutorium.js
@@ -1,40 +1,5 @@
-/** Tutorium-Data:
-  key: '1',                         Unique Key also displayed as Lesson number
-  title: 'Überblick und Historie',  Lesson Title
-  deadline: '10/30/2016',           Lession deadline (in Month/Day/Year Format) or null
-**/
-const Tutorium = [
-  {
-    key: '1',
-    title: 'Überblick und Historie',
-    deadline: '10/25/2016',
-  },
-  {
-    key: '2',
-    title: 'Zahlensysteme und Textkodierung',
-    deadline: null,
-  },
-  {
-    key: '3',
-    title: 'Business Computing',
-    deadline: null,
-  },
-  {
-    key: '4',
-    title: 'Programmierung – Grundlagen',
-    deadline: null,
-  },
-  {
-    key: '5',
-    title: 'Programmierung mit VBA',
-    deadline: null,
-  },
-  {
-    key: '6',
-    title: 'Rechnerarchitektur',
-    deadline: null,
-  },
-]
+import TutoriumData from 'data/TutoriumData';
+
 const convertStringToDate = (dateString, hours, minutes) => {
   const date = new Date(dateString)
   date.setHours(hours)
@@ -50,4 +15,4 @@ const convertDeadlineStringsToDates = (data) => {
   }
   return data;
 }
-export default Tutorium.map(convertDeadlineStringsToDates);
+export default TutoriumData.map(convertDeadlineStringsToDates);
---
+++ src/data/TutoriumData.js
@@ -0,0 +1,37 @@
+/** Tutorium-Data:
+  key: '1',                         Unique Key also displayed as Lesson number
+  title: 'Überblick und Historie',  Lesson Title
+  deadline: '10/30/2016',           Lession deadline (in Month/Day/Year Format) or null
+**/
+export default [
+  {
+    key: '1',
+    title: 'Überblick und Historie',
+    deadline: '10/25/2016',
+  },
+  {
+    key: '2',
+    title: 'Zahlensysteme und Textkodierung',
+    deadline: null,
+  },
+  {
+    key: '3',
+    title: 'Business Computing',
+    deadline: null,
+  },
+  {
+    key: '4',
+    title: 'Programmierung – Grundlagen',
+    deadline: null,
+  },
+  {
+    key: '5',
+    title: 'Programmierung mit VBA',
+    deadline: null,
+  },
+  {
+    key: '6',
+    title: 'Rechnerarchitektur',
+    deadline: null,
+  },
+]
