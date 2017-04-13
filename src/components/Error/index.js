bb7cc248ebd6d4e782c105ea8b4b205f5fc224de
Jonas Faber <dev.jonas.faber@gmail.com>
2017-04-13 03:02:56 +0200

    Update deps, add travis and add some documentation.

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './style.css';
 
-const Error = (props) => (
+const Error = props => (
   <main className="Error">
     <header>
       <h1>Error</h1>
     </header>
     <p>Error data not found.</p>
+    <p>Try to navigate to https://url/:spreadsheetID/:course</p>
   </main>
-)
+);
 
-export default Error
+export default Error;
